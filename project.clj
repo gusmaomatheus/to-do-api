c59728a36c32bc30a4759d2c33a71c83c99f0439
Matheus <mgusmao.dev@gmail.com>
2025-04-21 23:22:35 -0300

    build: add dependencies
    Libraries and frameworks that will be used in the project:
    
    - ring ->> http server
    - reitit ->> router
    - muuntaja ->> json encoding/decoding
    - next.jdbc ->> database access
    - postgresql ->> postgresql jdbc driver

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core "1.14.1"]
+                 [metosin/reitit "0.8.0"]
+                 [metosin/muuntaja "0.6.11"]
+                 [com.github.seancorfield/next.jdbc "1.3.1002"]
+                 [org.postgresql/postgresql "42.7.4"]]
   :main ^:skip-aot to-do-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
