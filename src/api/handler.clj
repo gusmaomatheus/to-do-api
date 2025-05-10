3458dff0190d2137cf5f51a651e01cccbf91baf7
Matheus <mgusmao.dev@gmail.com>
2025-05-10 01:25:48 -0300

    refactor(handler): refator http responses

--- src/api/handler.clj
+++ src/api/handler.clj
@@ -3,10 +3,10 @@
    [api.util :as util]))
 
 (defn http-200 [response]
-  {:status-code 200 :status-text "OK" :timestamp (util/->timestamp) :body {:errors [] :response response}})
+  {:status-code 200 :status-text "OK" :timestamp (str (util/->timestamp)) :body {:errors [] :response response}})
 
 (defn http-400 [message]
-  {:status-code 400 :status-text "BAD REQUEST" :timestamp (util/->timestamp) :body {:errors [message] :response {}}})
+  {:status-code 400 :status-text "BAD REQUEST" :timestamp (str (util/->timestamp)) :body {:errors [message] :response {}}})
 
 (defn http-500 [exception message]
-  {:status-code 500 :status-text "INTERNAL SERVER ERROR" :exception exception :timestamp (util/->timestamp) :body {:errors [message] :response {}}})
+  {:status-code 500 :status-text "INTERNAL SERVER ERROR" :exception exception :timestamp (str (util/->timestamp)) :body {:errors [message] :response {}}})
