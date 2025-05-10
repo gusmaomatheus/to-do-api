5c1d3ca0c7c46a0913f276721bcdd0c1fe53b0df
Matheus <mgusmao.dev@gmail.com>
2025-05-10 00:44:22 -0300

    style: correct grammar

--- src/api/handler.clj
+++ src/api/handler.clj
@@ -6,7 +6,7 @@
   {:status-code 200 :status-text "OK" :timestamp (util/->timestamp) :body {:errors [] :response response}})
 
 (defn http-400 [message]
-  {:status-code 400 :status-text "BAD REQUEST" :timestamp (util/->timestamp) :body {:erros [message] :response {}}})
+  {:status-code 400 :status-text "BAD REQUEST" :timestamp (util/->timestamp) :body {:errors [message] :response {}}})
 
 (defn http-500 [exception message]
   {:status-code 500 :status-text "INTERNAL SERVER ERROR" :exception exception :timestamp (util/->timestamp) :body {:errors [message] :response {}}})
