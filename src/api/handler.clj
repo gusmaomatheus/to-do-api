2c5d0fd99fb53f4191cd873fda92bdc7291f6ae9
Matheus <mgusmao.dev@gmail.com>
2025-05-10 00:18:13 -0300

    refactor(handler): change http response format

--- src/api/handler.clj
+++ src/api/handler.clj
@@ -3,7 +3,7 @@
    [api.util :as util]))
 
 (defn http-200 [response]
-  {:status-code 200 :status-text "OK" :timestamp (util/->timestamp) :body {:errors [] :response [response]}})
+  {:status-code 200 :status-text "OK" :timestamp (util/->timestamp) :body {:errors [] :response response}})
 
 (defn http-500 [exception message]
-  {:status-code 500 :status-text "Internal Server Error" :exception exception :timestamp (util/->timestamp) :body {:erros [message] :response []}})
+  {:status-code 500 :status-text "Internal Server Error" :exception exception :timestamp (util/->timestamp) :body {:errors [message] :response {}}})
