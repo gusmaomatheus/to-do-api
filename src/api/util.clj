f834ecb5e9f14b5af2e3980a36f448a627361276
Matheus <mgusmao.dev@gmail.com>
2025-05-09 23:51:24 -0300

    feat(util): create string utils

--- src/api/util.clj
+++ src/api/util.clj
@@ -1,8 +1,18 @@
 (ns api.util
+  (:require
+   [clojure.string :as string])
   (:import
    [java.time LocalDate]))
 
-;; Date and Time
+;; date and time
 
 (defn ->timestamp []
   (LocalDate/now))
+
+;; string
+
+(defn not-empty-str? [s]
+  (and (string? s) (not (string/blank? s))))
+
+(defn lower-str [s]
+  (string/lower-case s))
