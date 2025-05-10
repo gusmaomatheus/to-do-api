e9d48be1fdf7a8dc96b927a547d73fa6973fc8b1
Matheus <mgusmao.dev@gmail.com>
2025-05-09 23:28:57 -0300

    feat: create middleware for handling with exceptions

---
+++ src/api/middleware.clj
@@ -0,0 +1,13 @@
+(ns api.middleware
+  (:require
+   [api.handler :as handler]))
+
+(def wrap-exception
+  {:name ::exception
+   :description "Internal error."
+   :wrap (fn [handler]
+           (fn [request]
+             (try
+               (handler request)
+               (catch Exception exception
+                 (handler/http-500 exception "The request could not be executed.")))))})
