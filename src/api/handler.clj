a86145ecf56e409ff586c2ac4749d2ce69c11a47
Matheus <mgusmao.dev@gmail.com>
2025-05-09 22:59:45 -0300

    feat(handler): create htpp-500 response

--- src/api/handler.clj
+++ src/api/handler.clj
@@ -4,3 +4,6 @@
 
 (defn http-200 [response]
   {:status-code 200 :status-text "OK" :timestamp (util/->timestamp) :body {:errors [] :response [response]}})
+
+(defn http-500 [exception message]
+  {:status-code 500 :status-text "Internal Server Error" :exception exception :timestamp (util/->timestamp) :body {:erros [message] :response []}})
