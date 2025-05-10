4b0af6e96c98a265bf615bfa8bcf968878ec9126
Matheus <mgusmao.dev@gmail.com>
2025-05-10 01:50:28 -0300

    feat(env): create postgres env's

--- src/api/env.clj
+++ src/api/env.clj
@@ -1,3 +1,11 @@
 (ns api.env)
 
+;; API
 (defn server-port [] (Integer/parseInt (or (System/getenv "SERVER_PORT") "3000")))
+
+;; POSTGRES
+
+(defn postgres-port [] (Integer/parseInt (or (System/getenv "POSTGRES_PORT") "5432")))
+(defn postgres-db [] (String/valueOf (or (System/getenv "POSTGRES_DB") "localhost")))
+(defn postgres-user [] (String/valueOf (or (System/getenv "POSTGRES_USER") "user")))
+(defn postgres-password [] (String/valueOf (or (System/getenv "POSTGRES_PASSWORD") "1234")))
