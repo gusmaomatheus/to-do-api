e795fec75af2db76f1ed01f1f55daac58e238431
Matheus <mgusmao.dev@gmail.com>
2025-05-11 17:43:28 -0300

    revert: revert last commit

--- src/api/env.clj
+++ src/api/env.clj
@@ -2,10 +2,3 @@
 
 ;; API
 (defn server-port [] (Integer/parseInt (or (System/getenv "SERVER_PORT") "3000")))
-
-;; POSTGRES
-
-(defn postgres-port [] (Integer/parseInt (or (System/getenv "POSTGRES_PORT") "5432")))
-(defn postgres-db [] (String/valueOf (or (System/getenv "POSTGRES_DB") "localhost")))
-(defn postgres-user [] (String/valueOf (or (System/getenv "POSTGRES_USER") "user")))
-(defn postgres-password [] (String/valueOf (or (System/getenv "POSTGRES_PASSWORD") "1234")))
